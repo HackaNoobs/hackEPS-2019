8750b34f769650c3ff6cdc221d9a1a751e6206e1
JoanPalau <joanp4l4u@gmail.com>
2019-11-23 18:33:51 +0100

    Added controller and service for devices

--- server/src/main/java/com/hackeps2019/server/domain/Device.java
+++ server/src/main/java/com/hackeps2019/server/domain/Device.java
@@ -14,6 +14,8 @@ public class Device {
     @Id
     private String id;
     private String name;
+    @Indexed(direction = IndexDirection.ASCENDING)
+    private String user;
     private ReportStatus status;
     @Indexed(direction = IndexDirection.ASCENDING)
     private List<Report> reports;
