a1f45cd3627ea928e855049cc2c7b22dd5d8897a
JoanPalau <joanp4l4u@gmail.com>
2019-11-23 17:24:44 +0100

    Added device repository and db seeder util

---
+++ server/src/main/java/com/hackeps2019/server/utils/DbSeeder.java
@@ -0,0 +1,36 @@
+package com.hackeps2019.server.utils;
+
+import com.hackeps2019.server.domain.Device;
+import com.hackeps2019.server.domain.Report;
+import com.hackeps2019.server.domain.ReportStatus;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class DbSeeder implements CommandLineRunner {
+
+    @Override
+    public void run(String... strings) throws Exception {
+
+        List<Report> reports = new ArrayList<>();
+
+        //Create a report as example
+        Report report1 = new Report();
+        report1.setId("report 1");
+        report1.setHum("50");
+        report1.setTemp("25");
+
+        // Add report to report list
+        reports.add(report1);
+
+        // Create device
+        Device test = new Device();
+        test.setId("test");
+        test.setName("Test");
+        test.setStatus(ReportStatus.ON);
+        test.setReports(reports);
+    }
+}
